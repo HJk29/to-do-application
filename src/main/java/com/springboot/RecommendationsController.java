dfa32c7aa65203ac4130ac54435bfdeeba0c2353
kimjunha <rlawnsgk0202@hs.ac.kr>
2024-08-02 15:12:22 +0900

    controller commit

--- src/main/java/com/springboot/RecommendationsController.java
+++ src/main/java/com/springboot/RecommendationsController.java
@@ -1,2 +1,13 @@
-package com.springboot;public class RecommendationsController {
+package com.springboot;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RecommendationsController {
+
+    @GetMapping("/")
+    public String helloWorld(){
+        return "To-do Application!";
+    }
 }
